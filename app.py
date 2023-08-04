60a1b011bcb17341d4387784538eb5ddabab4242
noviccio <mnovicio2001@gmail.com>
2023-08-04 16:41:52 -0400

    Added comments to app.py, created Dockerfile

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+#import psutil, which provides functions for retrieving
+#information about the system, such as CPU and memory utilization.
+#imports Flask class, which is used to create the Flask application,
+#and render_template, which is used to render HTML templates.
+
 import psutil
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) #creates new Flask application instance 
 
 @app.route("/")
 def index():
@@ -9,9 +14,9 @@ def index():
     mem_percent = psutil.virtual_memory().percent 
     Message = None 
     if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU or Memory Utilization detected. Please scale up"
-    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=Message)
+        Message = "High CPU or Memory Utilization detected. Please scale up" #sends warning message
+    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=Message) #
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0') #allows web app to be accessible externally, using localhost
